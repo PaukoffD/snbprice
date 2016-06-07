36c5e0193beefda161fe6d0c16301ace428c9fbf
xuliaP <paukov2967@gmail.com>
2016-06-07 16:31:44 +0300

    add profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :goods
+  resource :profile, only: [:show, :edit, :update], as: :user_profile
+
   root 'main#index'
   get 'menu', to: 'goods#menu'
 end
